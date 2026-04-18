55580812c77212ea34319422d94b8c21460a2c8e
Ratasi947 <tansi2012@yahoo.com>
2026-04-18 21:51:57 +0900

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -3,7 +3,7 @@ from supabase import create_client, Client
 
 # SẾP CẦN THAY THẾ 2 DÒNG DƯỚI ĐÂY BẰNG THÔNG TIN THẬT CỦA SẾP
 SUPABASE_URL = "https://czcxtrpgunuksiqvouhi.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6Y3h0cnBndW51a3NpcXZvdWhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MTgxMTAsImV4cCI6MjA5MDE5NDExMH0.QwpHKwsdW7ZpDhkHsI3BVgxYnTeg4Tm_8zPl-GE3uCo"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6Y3h0cnBndW51a3NpcXZvdWhpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NDYxODExMCwiZXhwIjoyMDkwMTk0MTEwfQ.nMgzrqYorrsVjm-d-S2Eo9BQkB3LKA7oYv1qULzUk90"
 
 # Khởi tạo kết nối
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
